ab2ebca010eea5ad4bc4bcad6d8ec77879918022
irwin felicien <ifelicien@gmail.com>
2021-01-16 01:28:40 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+
+    public void characterNames() {
+        System.out.println("Enter number of players");
+        int numbOfPlayer = input.nextInt();
+
+        if (numbOfPlayer <= 16) {
+            for (int i = 0; i < numbOfPlayer; i++) {
+
+                System.out.println("please enter character names: ");
+                String playerName = input.nextLine();
+            }
+        }
+        else{
+            System.out.println("invalid selection");
+        }
+    }
+}
